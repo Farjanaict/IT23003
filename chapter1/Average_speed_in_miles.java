bb18f244570f096b53d45654e6856cefdab3055e
Farjanaict <IT23003@mbstu.ac.bd>
2024-11-18 21:31:12 +0600

    Add files via upload

---
+++ chapter1/Average_speed_in_miles.java
@@ -0,0 +1,12 @@
+//Average speed in miles
+public class Average_speed_in_miles
+{
+public static void main(String[] args)
+{
+double s,t,v;
+s=14/1.6;
+t=((45/60)+(30/3600));
+v=(double)s/t;
+System.out.println("Average Speed in miles = "+v);
+}
+}
